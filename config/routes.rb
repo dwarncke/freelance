8b2112049fff7e31fbaff276a52cc5ebd5223578
dwarncke <daniel.warncke@gmail.com>
2020-07-10 21:09:40 +0000

    new links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'contact', to: 'home#new'
+  get 'portfolio', to: 'home#create'
+  get 'love', to: 'home#love'
+  get 'hate', to: 'home#hate'
   resources :users
 end
